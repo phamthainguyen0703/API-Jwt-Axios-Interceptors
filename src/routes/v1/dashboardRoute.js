37f7a637f12456e1c09f1efbb2128fec70d7e826
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-06-26 23:24:32 +0700

    Initial

---
+++ src/routes/v1/dashboardRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { dashboardController } from "~/controllers/dashboardController";
+
+const Router = express.Router();
+
+Router.route("/access").get(dashboardController.access);
+
+export const dashboardRoute = Router;
